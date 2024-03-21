78180f98217aa2eccf72da57b4466b28d1c81942
Arman-Mishra <armanmishra.sky@gmail.com>
2024-03-21 18:27:45 +0530

    Xcountries buildout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import FlagCard from "./FlagCard";
 
 function App() {
+  const [flags, setFlags] = useState([]);
+
+  useEffect(() => {
+    fetchApi(api_url);
+  }, []);
+
+  const api_url = `https://restcountries.com/v3.1/all`;
+
+  const fetchApi = async (url) => {
+    try {
+      const resp = await axios.get(url);
+      setFlags(resp.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {flags.map((item) => {
+        return (
+          <FlagCard
+            key={item.name.official}
+            name={item.name.common}
+            img_src={item.flags.svg}
+            alt={item.flags.alt}
+          />
+        );
+      })}
     </div>
   );
 }
